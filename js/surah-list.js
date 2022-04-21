e91509634fba59894123652d3f7317d4193271eb
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-21 19:07:40 +0600

    first commit

---
+++ js/surah-list.js
@@ -0,0 +1,237 @@
+// To add more song, just copy the following code and paste inside the array
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+//paste it inside the array as more as you want music then you don't need to do any other thing
+let allMusic = [{
+            name: "Al-Fatiha",
+            src: "1"
+        },
+        {
+            name: "Al-Baqarah",
+            src: "2"
+        },
+        {
+            name: "Al-ee-Imran",
+            src: "3"
+        },
+        {
+            name: "An-Nisa",
+            src: "4"
+        },
+        {
+            name: "Al-Ma'idah",
+            src: "5"
+        },
+        {
+            name: "Al-An'am",
+            src: "6"
+        },
+        {
+            name: "Al-A'raf",
+            src: "7"
+        },
+        {
+            name: "Al-Anfal",
+            src: "8"
+        },
+        {
+            name: "At-Tawbah",
+            src: "9"
+        },
+        {
+            name: "Yunus",
+            src: "10"
+        },
+        {
+            name: "Hud",
+            src: "11"
+        },
+        {
+            name: "Yusuf",
+            src: "12"
+        },
+        {
+            name: "Ar-Ra'd",
+            src: "13"
+        },
+        {
+            name: "Ibrahim",
+            src: "14"
+        },
+        {
+            name: "Al-Hijr",
+            src: "15"
+        },
+        {
+            name: "An-Nahl",
+            src: "16"
+        },
+        {
+            name: "Al-Isra",
+            src: "17"
+        },
+        {
+            name: "Al-Kahf",
+            src: "18"
+        },
+        {
+            name: "Maryam",
+            src: "19"
+        },
+        {
+            name: "Ta-Ha",
+            src: "20"
+        },
+        {
+            name: "Al-Anbiya",
+            src: "21"
+        },
+        {
+            name: "Al-Hajj",
+            src: "22"
+        },
+        {
+            name: "Al-Muminun",
+            src: "23"
+        },
+        {
+            name: "An-Nur",
+            src: "24"
+        },
+        {
+            name: "Al-Furqan",
+            src: "25"
+        },
+        {
+            name: "Ash-Shu'ara",
+            src: "26"
+        },
+        {
+            name: "An-Naml",
+            src: "27"
+        },
+        {
+            name: "Al-Qasas",
+            src: "28"
+        },
+        {
+            name: "Al-Ankabut",
+            src: "29"
+        },
+        {
+            name: "Ar-Rum",
+            src: "30"
+        },
+        {
+            name: "Luqman",
+            src: "31"
+        },
+        {
+            name: "As-Sajdah",
+            src: "32"
+        },
+        {
+            name: "Al-Ahzab",
+            src: "33"
+        },
+        {
+            name: "Saba",
+            src: "34"
+        },
+        {
+            name: "Fatir",
+            src: "35"
+        },
+        {
+            name: "Ya-Sin",
+            src: "36"
+        },
+        {
+            name: "As-Saffat",
+            src: "37"
+        },
+        {
+            name: "Sad",
+            src: "38"
+        },
+        {
+            name: "Az-Zumar",
+            src: "39"
+        },
+        {
+            name: "Ghafir",
+            src: "40"
+        },
+        {
+            name: "Fussilat",
+            src: "41"
+        },
+        {
+            name: "Ash-Shura",
+            src: "42"
+        },
+        {
+            name: "Az-Zukhruf",
+            src: "43"
+        },
+        {
+            name: "Ad-Dukhan",
+            src: "44"
+        },
+        {
+            name: "Al-Jathiyah",
+            src: "45"
+        },
+        {
+            name: "Al-Ahqaf",
+            src: "46"
+        },
+        {
+            name: "Muhammad",
+            src: "47"
+        },
+        {
+            name: "Al-Fath",
+            src: "48"
+        },
+        {
+            name: "Al-Hujurat",
+            src: "49"
+        },
+        {
+            name: "Qaf",
+            src: "50"
+        },
+        {
+            name: "Adh-Dhariyat",
+            src: "51"
+        },
+        {
+            name: "At-Tur",
+            src: "52"
+        },
+        {
+            name: "An-Najm",
+            src: "53"
+        },
+        {
+            name: "Al-Qamar",
+            src: "54"
+        },
+        {
+            name: "Ar-Rahman",
+            src: "55"
+        },
+        {
+            name: "Al-Waqi'ah",
+            src: "56"
+        },
+        {
+            name: "Al-Hadid",
+            src: "57"
+        },
+   ];
